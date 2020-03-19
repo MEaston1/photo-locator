baaf5b941261ac6f475f1eb1f9ee899670de3ac2
MEaston1 <45512046+MEaston1@users.noreply.github.com>
2020-03-19 15:58:28 +0000

    recyclerview

--- PhotoLocator/app/src/main/java/com/apps/photolocator/models/Location.kt
+++ PhotoLocator/app/src/main/java/com/apps/photolocator/models/Location.kt
@@ -1,6 +1,11 @@
 package com.apps.photolocator.models
 
-class Location (val id: String, val name: String, val country: String, val lat: String, val long: String, val locationImageUrl: String) {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Location (val id: String, val name: String, val country: String, val lat: String, val long: String, val locationImageUrl: String) :
+    Parcelable {
 
     constructor(): this ("", "", "", "", "",""){
     }
